f023575f133fe578747d43add58d5174246cef4b
Robert Hromada <33429366+bobes1975@users.noreply.github.com>
2018-03-30 20:48:16 +0200

    Project 06 - ND - NewsApp 1st Stage for Android Nanodegree - Android Basic
    Project 06 - ND - NewsApp 1st Stage for Android Nanodegree - Android Basic

---
+++ app/src/main/java/com/example/android/newsappver1/NewsLoader.java
@@ -0,0 +1,47 @@
+package com.example.android.newsappver1;
+
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    private String mUrl;
+
+    /**
+     * New NewsLoader.
+     *
+     * @param context of the activity
+     * @param url     url for
+     */
+    public NewsLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    // Background thread.
+
+    @Override
+    public List<News> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Make the network request and after extraction of list of news parse the response.
+        List<News> News = null;
+        try {
+            News = NewsUtils.fetchNewsData(mUrl);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return News;
+    }
+}
+
